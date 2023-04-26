a606c35a5d0245fb4ab1f9a888194181ad1ab0af
Rob <rob@wildspark.nl>
2023-04-26 11:02:15 +0200

    Listeners for ready and interaction creation

---
+++ src/listeners/interactionCreate.ts
@@ -0,0 +1,54 @@
+import {
+  Client,
+  ClientApplication,
+  CommandInteraction,
+  Events,
+  Interaction,
+} from "discord.js";
+import { findCommandByName } from "../utils/commandUtils";
+import { Command } from "../Command";
+
+const handleCommand = async (
+  command: Command,
+  interaction: CommandInteraction
+): Promise<void> => {
+  try {
+    await command.run(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: `There was an error while executing ${interaction?.commandName}!`,
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: `There was an error while executing ${interaction?.commandName}!`,
+        ephemeral: true,
+      });
+    }
+  }
+};
+
+export default (client: Client): void => {
+  client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+    if (
+      !interaction.isChatInputCommand() ||
+      !(interaction instanceof CommandInteraction) ||
+      !(client.application instanceof ClientApplication)
+    ) {
+      return;
+    }
+
+    const command = findCommandByName(interaction.commandName);
+
+    if (!command) {
+      console.error(
+        `No command matching ${interaction.commandName} was found.`
+      );
+      return;
+    }
+
+    await handleCommand(command, interaction);
+  });
+};
---
+++ src/listeners/ready.ts
@@ -0,0 +1,19 @@
+import { Client, ClientApplication } from "discord.js";
+import { Commands } from "../Command";
+
+export default (client: Client): void => {
+  client.on("ready", async () => {
+    if (
+      !client.user ||
+      !client.application ||
+      !(client.application instanceof ClientApplication)
+    ) {
+      return;
+    }
+
+    console.log("Registering commands ...");
+    await client.application.commands.set(Commands);
+
+    console.log(`${client.user?.username} is online`);
+  });
+};
