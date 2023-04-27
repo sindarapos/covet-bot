a5977955fa9e96216ad98be119094967c3e4927f
Rob <rob@wildspark.nl>
2023-04-27 22:54:01 +0200

    Summary format utility function

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,5 +1,8 @@
-export const fuzzyMatch = (pattern: string, str: string) => {
-  pattern = `.*${ pattern.split('').join('.*') }.*`;
-  const re = new RegExp(pattern);
-  return re.test(str);
+export const summaryFormatter = (list: string[], limit = 3, separator = ', '): string => {
+  if (list.length <= limit) {
+    return list.join(separator);
+  }
+
+  const firstItems = list.slice(0, limit);
+  return `${firstItems.join(separator)} and ${list.length - limit} more`;
 };
