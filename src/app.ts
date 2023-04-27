4d34184e2af3a393bfbe9a9219909464eb4a02a3
Rob <rob@wildspark.nl>
2023-04-28 00:35:49 +0200

    Update messaging

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { Client } from 'discord.js';
 import ready from './listeners/ready';
 import interactionCreate from './listeners/interactionCreate';
 
-console.log('Bot is starting...');
+console.log('Bot is starting ...');
 const client = new Client({
   intents: [],
 });
@@ -11,6 +11,6 @@ const client = new Client({
 console.log('Logging in ...');
 client.login(process.env.DISCORD_TOKEN);
 
-console.log('Registering listeners:');
+console.log('Registering listeners ...');
 ready(client);
 interactionCreate(client);
