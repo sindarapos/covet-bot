cef99710a9a2f9d640e8acd352c6c74d7aaf2bdc
Rob <rob@wildspark.nl>
2023-04-29 01:19:46 +0200

    Initialize database

---
+++ src/configuration/database.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize(process.env.DB_URL ?? '', {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+  },
+});
