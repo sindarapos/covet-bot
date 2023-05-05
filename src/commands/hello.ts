d5ca294c257d02f3fefa8991c085477fdcf6e6bf
Rob <rob@wildspark.nl>
2023-05-05 11:09:59 +0200

    Upsert userid on hello command

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,14 +1,18 @@
 import { Command, CommandName } from '../Command';
+import { UserModel } from '../configuration/models/userModel';
 
 export const hello: Command = {
   name: CommandName.Hello,
   description: 'Returns a greeting',
   run: async (interaction) => {
-    const content = 'Hello there!';
+    const { user: { username, id } } = interaction;
 
-    await interaction.reply({
-      ephemeral: true,
-      content,
+    await interaction.deferReply({ ephemeral: true });
+
+    const [user] = await UserModel.upsert({ discordUserId: id, username });
+    await interaction.editReply({
+      content: `Hello there! ${username}, you've been registered as ${user?.id}`,
     });
+
   },
 };
