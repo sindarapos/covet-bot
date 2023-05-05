e1b5a7df04f71072f751aaa429d5ae8e47242135
Rob <rob@wildspark.nl>
2023-05-05 14:51:09 +0200

    Check if response is actually a steam app

--- src/SteamApp.ts
+++ src/SteamApp.ts
@@ -2,3 +2,49 @@ export interface SteamApp {
   id: number;
   name: string;
 }
+
+export interface SteamAppList {
+  appList: {
+    apps: SteamApp[];
+  };
+}
+
+function isRecord(element: unknown): element is Record<string, unknown> {
+  return !!element && Object.getPrototypeOf(element) === Object.prototype;
+}
+
+function isRecordWithProperties<K extends PropertyKey[]>(
+  element: unknown,
+  prop: K,
+): element is Record<K[number], unknown> {
+  if (!isRecord(element)) {
+    return;
+  }
+  return prop in element;
+}
+
+function isSteamApp(element: unknown): element is SteamApp {
+  return isRecordWithProperties(element, ['id' as const, 'name' as const]);
+}
+
+export function isSteamAppList(element: unknown): element is SteamAppList {
+  if (!isRecordWithProperties(element, ['appList' as const])) {
+    return;
+  }
+  const { appList } = element;
+  if (!isRecordWithProperties(appList, ['apps' as const])) {
+    return;
+  }
+  const { apps } = appList;
+  if (!Array.isArray(apps)) {
+    return;
+  }
+
+  // Only check the first member for speed
+  const app: unknown = apps[0];
+  if (!app) {
+    return true;
+  }
+
+  return isSteamApp(app);
+}
--- src/utils/steamUtils.ts
+++ src/utils/steamUtils.ts
@@ -1,9 +1,14 @@
 import Fuse from 'fuse.js';
-import { SteamApp } from '../SteamApp';
+import { isSteamAppList, SteamApp } from '../SteamApp';
 
 export const querySteamApps = (apps: SteamApp[], query: string): SteamApp[] => {
-  const fuse = new Fuse(apps, { keys: ['name'], shouldSort: true, findAllMatches: true, threshold: 0.1 });
-  return fuse.search(query)?.map((fuseResult) => fuseResult.item);
+  const fuse = new Fuse(apps, {
+    keys: ['name'],
+    shouldSort: true,
+    findAllMatches: true,
+    threshold: 0.1,
+  });
+  return fuse.search(query).map((fuseResult) => fuseResult.item);
 };
 
 export const fetchSteamApps = async (): Promise<SteamApp[]> => {
@@ -11,11 +16,13 @@ export const fetchSteamApps = async (): Promise<SteamApp[]> => {
     const response = await fetch(
       'https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json',
     );
-    const data = await response.json();
-    return data?.applist?.apps ?? [];
-  }
-  catch (e) {
-    throw new Error(`I was unable to contact steam: ${e}`);
+    const data = (await response.json()) as unknown;
+    if (!isSteamAppList(data)) {
+      return [];
+    }
+    return data.appList.apps;
+  } catch (e) {
+    throw new Error(`I was unable to contact steam`);
   }
 };
 
