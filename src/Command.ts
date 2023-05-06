278913e1f4c3019c6ed21509c39edae40e19b151
Rob <rob@wildspark.nl>
2023-05-07 00:14:03 +0200

    Add autocomplete to Command interface

--- src/Command.ts
+++ src/Command.ts
@@ -1,4 +1,8 @@
-import { ChatInputApplicationCommandData, CommandInteraction } from 'discord.js';
+import {
+  AutocompleteInteraction,
+  ChatInputApplicationCommandData,
+  ChatInputCommandInteraction,
+} from 'discord.js';
 import { hello } from './commands/hello';
 import { findGame } from './commands/findGame';
 
@@ -7,12 +11,10 @@ export const enum CommandName {
   FindGame = 'find',
 }
 
-export interface BaseCommand extends ChatInputApplicationCommandData {
+export interface Command extends ChatInputApplicationCommandData {
   name: CommandName;
+  run: (interaction: ChatInputCommandInteraction) => Promise<void>;
+  autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
 }
-
-type Executor = (interaction: CommandInteraction) => Promise<void>;
-
-export type Command = BaseCommand & ({ run: Executor } | { autocomplete: Executor });
 
 export const commands: Command[] = [hello, findGame];
