c0a539ce2db4b9d11cd4d614f12419994950d52b
Rob <rob@wildspark.nl>
2023-05-07 00:15:44 +0200

    Add autocomplete for finding games

--- src/commands/findGame.ts
+++ src/commands/findGame.ts
@@ -3,6 +3,8 @@ import { summaryFormatter } from '../utils/stringUtils';
 import { findSteamApps } from '../utils/steamUtils';
 import { SteamApp } from '../SteamApp';
 
+const NUM_AUTOCOMPLETE_SUGGESTIONS = 20;
+
 const generateContent = (apps: SteamApp[], query: string): string => {
   // no game found
   if (apps.length === 0) {
@@ -26,6 +28,7 @@ const options: Command['options'] = [
     name: 'query',
     description: 'The name of the game you are trying to find.',
     required: true,
+    autocomplete: true,
   },
 ];
 
@@ -33,6 +36,17 @@ export const findGame: Command = {
   name: CommandName.FindGame,
   description: 'Finds a game in the steam store.',
   options,
+  autocomplete: async (interaction) => {
+    const focussedValue = interaction.options.getFocused();
+    const apps = await findSteamApps(focussedValue);
+    const autocompleteOptions = apps
+      .slice(0, NUM_AUTOCOMPLETE_SUGGESTIONS)
+      .map((app) => ({
+        name: app.name,
+        value: app.name,
+      }));
+    await interaction.respond(autocompleteOptions);
+  },
   run: async (interaction) => {
     const query = interaction.options.get('query')?.value;
 
