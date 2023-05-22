dadea1bea945da82c0b0dbb407318b7031bb3b5f
Rob <rob@wildspark.nl>
2023-05-22 22:57:28 +0200

    Utils to generate a list of games

--- src/utils/gameUtils.ts
+++ src/utils/gameUtils.ts
@@ -1,5 +1,6 @@
 import {
   ActionRowBuilder,
+  bold,
   ButtonBuilder,
   ButtonStyle,
   ChatInputCommandInteraction,
@@ -9,6 +10,9 @@ import { SteamAppDetail } from '../SteamAppDetail';
 import { findSteamAppDetails } from '../services/steamService';
 import { generateSteamAppEmbed } from './steamUtils';
 import { MessageActionRowComponentBuilder } from '@discordjs/builders';
+import { GameModel } from '../configuration/models/game.model';
+import { CategoryModel } from '../configuration/models/category.model';
+import moment from 'moment/moment';
 
 export const enum ButtonCustomIds {
   confirm = 'confirm',
@@ -61,4 +65,36 @@ export const findAndDisplaySteamAppDetails = async (
     components: [row],
   });
   return [details, message];
+};
+const generateGameCategoryIcons = (categories?: CategoryModel[]): string => {
+  const isMultiplayer = categories?.some(
+    ({ description }) => description === 'Multi-player',
+  );
+  const isCoop = categories?.some(({ description }) => description === 'Co-op');
+  const isPvP = categories?.some(({ description }) => description === 'PvP');
+  const multiplayerIcon = isMultiplayer ? ':family:' : '';
+  const coopIcon = isCoop ? ':two_hearts:' : '';
+  const pvpIcon = isPvP ? ':crossed_swords:' : '';
+
+  return `${multiplayerIcon} ${coopIcon} ${pvpIcon}`;
+};
+const generateGameListItem = ({
+  name,
+  releaseDate,
+  owners,
+  genres,
+  categories,
+}: GameModel): string => {
+  const releaseDateMoment = moment(releaseDate);
+  const isPastRelease = releaseDateMoment.isBefore(moment());
+  const fromNow = releaseDateMoment.fromNow();
+  const username = owners?.[0]?.username ?? 'nobody';
+  const genreSummary = genres?.map(({ description }) => description)?.join(', ') ?? '';
+  const categoryIcons = generateGameCategoryIcons(categories);
+  return `${bold(name)} ${categoryIcons}\n${genreSummary}\nRelease${
+    isPastRelease ? 'd' : 's'
+  } ${fromNow} · Added by ${username}`;
+};
+export const generateGameList = (games: GameModel[]): string => {
+  return games.map(generateGameListItem).join('\n\r');
 };
