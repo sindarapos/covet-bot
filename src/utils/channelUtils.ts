5d24c0d3ef4279ce9483714b0e2fe2c17e2ffe2e
Rob <rob@wildspark.nl>
2023-05-29 00:40:12 +0200

    Split up channelsByName command

--- src/utils/channelUtils.ts
+++ src/utils/channelUtils.ts
@@ -1,13 +1,16 @@
 import { Channel, Client, TextBasedChannel } from 'discord.js';
 
-export async function channelsByName(client: Client, channelName = 'game-announcements') {
+export const channelIdsByName = async (
+  client: Client,
+  channelName = 'game-announcements',
+) => {
   const oAuth2Guilds = await client.guilds.fetch();
   const guilds = await Promise.all(
     oAuth2Guilds.map(async (oAuth2Guild) => oAuth2Guild.fetch()),
   );
   const guildChannels = await Promise.all(guilds.map(({ channels }) => channels.fetch()));
 
-  const announcementChannelIds = guildChannels
+  return guildChannels
     .map((guildChannels) => {
       const guildChannel = guildChannels.find(
         (channel) => channel?.name.toLowerCase() === channelName,
@@ -15,7 +18,13 @@ export async function channelsByName(client: Client, channelName = 'game-announc
       return guildChannel?.id;
     })
     .filter((id): id is string => !!id);
+};
 
+export const channelsByName = async (
+  client: Client,
+  channelName = 'game-announcements',
+) => {
+  const announcementChannelIds = await channelIdsByName(client, channelName);
   const announcementChannels = await Promise.all(
     announcementChannelIds.map(async (id) => {
       return await client.channels.fetch(id);
@@ -24,4 +33,4 @@ export async function channelsByName(client: Client, channelName = 'game-announc
   return announcementChannels
     .filter((channel): channel is Channel => !!channel)
     .filter((channel): channel is TextBasedChannel => channel.isTextBased());
-}
+};
